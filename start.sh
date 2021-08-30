a5698efa51f4985232f89091a57dd92592399d0c
NovaGaming1964 <40416745+NovaGaming1964@users.noreply.github.com>
2021-08-30 15:59:25 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/NovaGaming1964/TGVCMucisBot.git /TG-Music
+git clone https://github.com/NovaGaming1964/TG-Music-Player.git /TG-Music
 cd /TG-Music
 echo "Starting Bot...."
 python3 main.py
